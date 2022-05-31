c8695320336cc8cd8e35f452bfb9e8597dd32ba9
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-31 15:26:45 -0400

    Added more style and started on calculator functionality

--- script.js
+++ script.js
@@ -26,6 +26,52 @@ const operate = function(operator, a, b) {
         return operator(a, b);
     }
     return "INVALID SELECTION"
-}
+};
 
 // Variable to store display numbers
+const display = document.querySelector("#display");
+
+// Functions for buttons
+const writeToDisplay = function(value) {
+    display.textContent += value;
+};
+
+const clearDisplay = function() {
+    display.textContent = "";
+};
+
+// Add events to buttons
+const numberButtons = document.querySelectorAll('#number');
+numberButtons.forEach(button => {
+    const dataValue = button.getAttribute('data-key');
+    button.addEventListener('click', () => {
+        if (
+            display.textContent === "+" ||
+            display.textContent === "-" ||
+            display.textContent === "*" ||
+            display.textContent === "/"
+        ) {
+            clearDisplay();
+        }
+        writeToDisplay(dataValue);
+    });
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearDisplay);
+
+const equalsButton = document.querySelector('#equals');
+clearButton.addEventListener('click', () => {
+    const result = operate(operator, a, b);
+    writeToDisplay(result);
+});
+
+const operatorButtons = document.querySelectorAll('#operator');
+operatorButtons.forEach(button => {
+    const operatorValue = button.getAttribute('data-key');
+    const textValue = button.textContent;
+    button.addEventListener('click', () => {
+        clearDisplay();
+        writeToDisplay(textValue);
+    });
+});
